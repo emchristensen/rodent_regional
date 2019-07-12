31d7a75f5601df2cce01969dcaf4622e5c4f9ce0
emchristensen <erica.christensen@weecology.org>
2019-07-12 16:14:07 -0600

    start code for downloading MODIS and GIMMS NDVI data

---
+++ get_MODIS_NDVI.R
@@ -0,0 +1,8 @@
+#' Script to download MODIS NDVI data for portal area
+#' EMC 7/11/19
+#' 
+#' MODIS r package info https://cran.r-project.org/web/packages/MODIS/MODIS.pdf
+#' MODIStsp r package 
+
+library(MODIStsp)
+MODIStsp() # opens a GUI. I saved the settings as PortalMODIS.json
