6598e50d1173ba013f12bed52dbe46a626ae6ae0
Erica Christensen <erica@ericas-air.wireless-guest.unc.edu>
2019-07-22 11:14:59 -0400

    add csv of trapping effort; added code to get trapping effort

--- get_portal_rodent_data.R
+++ get_portal_rodent_data.R
@@ -14,20 +14,36 @@ data_tables <- load_rodent_data(path='.')
 rodent_data = data_tables$rodent_data
 plots_table = data_tables$plots_table
 species_table = data_tables$species_table
+trapping_table = data_tables$trapping_table
 
 # merge rodent data with support tables
 rodent_plots = merge(rodent_data, plots_table[,c('year','month','plot','treatment')], 
-                     by=c('year','month','plot'), all.x=T) %>%
-  merge(species_table[,c('species','scientificname','censustarget')], by='species', all.x=T)
+                     by=c('year','month','plot'), all=T) %>%
+  merge(species_table[,c('species','scientificname','censustarget')], by='species', all.x=T) %>%
+  merge(trapping_table, by=c('year','month','day','period','plot'), all=T)
 
 
 # filter for: only control plots, only target species, only October censuses
 portal_octobers = rodent_plots %>%
   dplyr::filter(treatment=='control', month==10, censustarget==1) %>%
+  dplyr::arrange(period, plot)
+
+# divide information:
+#   rodent data
+portal_octobers_rodents = portal_octobers %>%
   dplyr::select(recordID, year, month, day, period, plot, treatment, note1, stake, species, 
                 scientificname, sex, reprod, age, testes, vagina, pregnant, nipples, lactation,
                 hfl, wgt, tag, note2, ltag, note3, prevrt, prevlet, nestdir, neststk, note4, note5) %>%
-  dplyr::arrange(period, plot)
+  dplyr::arrange(recordID)
+#   trapping info
+portal_octobers_trapping = portal_octobers %>%
+  dplyr::select(year, month, day, period, plot, sampled, effort) %>%
+  unique() %>%
+  dplyr::group_by(period, year) %>%
+  summarize(num_plots=length(unique(plot)),
+            num_traps=sum(effort))
+  
 
 # write to csv
-write.csv(portal_octobers, "portal_rodent_controlplots_1977_2017.csv")
+write.csv(portal_octobers_rodents, "portal_rodent_controlplots_1977_2017.csv")
+write.csv(portal_octobers_trapping, "portal_trapping_effort_1977_2017.csv")
